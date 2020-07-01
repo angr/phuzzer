39959a8974fb00cca5f66c8d9bbfb0fea307bf5d
ErikT <erik.trickel@asu.edu>
2020-07-01 10:39:09 -0700

    dictionary creation was missing after last fix.

--- phuzzer/phuzzers/__init__.py
+++ phuzzer/phuzzers/__init__.py
@@ -220,9 +220,11 @@ class Phuzzer:
     def create_dictionary(self):
         try:
             import angr
+            self.create_dictionary_angr()
         except ImportError:
             try:
                 import elftools
+                self.create_dictionary_elftools()
             except ImportError:
                 raise ModuleNotFoundError("Cannot create a dictionary without angr or elftools being installed")
 
