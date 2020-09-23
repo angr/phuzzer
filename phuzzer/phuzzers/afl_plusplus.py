8e83007fe18c09b472707e523485f86afeedb5ff
mahaloz <zbasque@asu.edu>
2020-09-23 16:26:37 -0700

    added affinity option

--- phuzzer/phuzzers/afl_plusplus.py
+++ phuzzer/phuzzers/afl_plusplus.py
@@ -1,6 +1,7 @@
 from .afl import AFL
 import logging
 import os
+import subprocess
 
 l = logging.getLogger("phuzzer.phuzzers.afl")
 
@@ -17,3 +18,28 @@ class AFLPlusPlus(AFL):
         self.afl_bin_dir = '/phuzzers/AFLplusplus/'
         afl_bin_path = os.path.join(self.afl_bin_dir, "afl-fuzz")
         return afl_bin_path
+
+    def _start_afl_instance(self, instance_cnt=0):
+
+        args, fuzzer_id = self.build_args()
+        my_env = os.environ.copy()
+
+        if "AFL_SET_AFFINITY" in my_env:
+            core_num = int(my_env["AFL_SET_AFFINITY"])
+            core_num += instance_cnt
+            args = args[0] + [f"-b {core_num}"] + args[1:]
+
+
+        self.log_command(args, fuzzer_id, my_env)
+
+        logpath = os.path.join(self.work_dir, fuzzer_id + ".log")
+        l.debug("execing: %s > %s", ' '.join(args), logpath)
+
+        if "afl_set_affinity" in my_env:
+            tempint = int(my_env["AFL_SET_AFFINITY"])
+            tempint += instance_cnt
+            my_env["AFL_SET_AFFINITY"] = str(tempint)
+
+        with open(logpath, "w") as fp:
+            return subprocess.Popen(args, stdout=fp, stderr=fp, close_fds=True, env=my_env)
+
