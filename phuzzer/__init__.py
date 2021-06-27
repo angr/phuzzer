7c4f6bd35ddfa1995ddc93214967edd0979ef291
Yan <yans@yancomm.net>
2021-06-26 23:26:39 -0700

    fix my typo

--- phuzzer/__init__.py
+++ phuzzer/__init__.py
@@ -5,6 +5,6 @@ from .showmap import Showmap
 from .extensions import *
 from .hierarchy import *
 from .phuzzers.afl import AFL
-from .phuzzers.afl import AFLPlusPlus
+from .phuzzers.afl_plusplus import AFLPlusPlus
 from .phuzzers.afl_multicb import AFLMultiCB
 from .phuzzers.afl_ijon import AFLIJON
