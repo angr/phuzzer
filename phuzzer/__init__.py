8fb6465fd2d9d9a9e623844852b245d128b007a3
Yan <yans@yancomm.net>
2021-06-26 23:21:26 -0700

    import AFLPlusPlus

--- phuzzer/__init__.py
+++ phuzzer/__init__.py
@@ -5,5 +5,6 @@ from .showmap import Showmap
 from .extensions import *
 from .hierarchy import *
 from .phuzzers.afl import AFL
+from .phuzzers.afl import AFLPlusPlus
 from .phuzzers.afl_multicb import AFLMultiCB
 from .phuzzers.afl_ijon import AFLIJON
